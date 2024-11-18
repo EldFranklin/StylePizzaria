8845a8cf9e9a5d491ded8c99064f2bdaf1b337ef
ph3523 <ph.barroso3523@gmail.com>
2024-11-18 03:39:40 -0300

    adiciona arquivo de estilos CSS e cria template HTML para o cardápio com tradução para o tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'Quicksand': ['"Quicksand"', 'sans-serif'],
-      
+      'Quicksand': ['"Quicksand"', 'sans-serif'],      
     },
   },
   plugins: [],
